58c470b05c9e903cbd790d54be7a935fbff5c4df
Karthik <namburu.karthik@gmail.com>
2013-05-15 21:23:14 +0530

    first commit

---
+++ FourthViewController.m
@@ -0,0 +1,38 @@
+//
+//  FourthViewController.m
+//  bheckerTabbedApp
+//
+//  Created by Karthik on 15/05/13.
+//  Copyright (c) 2013 Karthik. All rights reserved.
+//
+
+#import "FourthViewController.h"
+
+@interface FourthViewController ()
+
+@end
+
+@implementation FourthViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	// Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
---
+++ ThirdViewController.m
@@ -0,0 +1,38 @@
+//
+//  ThirdViewController.m
+//  bheckerTabbedApp
+//
+//  Created by Karthik on 15/05/13.
+//  Copyright (c) 2013 Karthik. All rights reserved.
+//
+
+#import "ThirdViewController.h"
+
+@interface ThirdViewController ()
+
+@end
+
+@implementation ThirdViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+    // Do any additional setup after loading the view from its nib.
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
